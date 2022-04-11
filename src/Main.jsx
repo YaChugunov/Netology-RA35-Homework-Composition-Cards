41b241a334513b0c6e6f7549ad4c8078d4989e45
YaChugunov <informed.looser@gmail.com>
2022-04-11 23:32:08 +0300

    1 file modified

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,22 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Card(props) {
   return (
     <>
-      <div className="card">
-        <img src={props.image} className="card-img-top" alt="" />
-        <div className="card-body">
-          <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">{props.content}</p>
-          <a href="#" className="btn btn-primary">
-            {props.button}
-          </a>
-        </div>
-      </div>
-      <div>Hello {props.children}</div>
+      <div className="card">{props.children}</div>
     </>
   );
 }
 
-export default function Main() {
+export default function Main(props) {
   const cardImage =
     'https://mebel-go.ru/mebelgoer/9663brush-colorful-graphic-bright-3840x2400.jpg';
   const cardTitles = ['Card title', 'Special title treatment'];
@@ -28,13 +18,23 @@ export default function Main() {
     'With supporting text below as a natural lead-in to additional content.',
   ];
   const cardButton = 'Go somewhere';
-
   return (
-    <Card
-      image={cardImage}
-      title={cardTitles[0]}
-      content={cardContents[0]}
-      button={cardButton}
-    ></Card>
+    <Card>
+      <img
+        src="https://mebel-go.ru/mebelgoer/9663brush-colorful-graphic-bright-3840x2400.jpg"
+        className="card-img-top"
+        alt=""
+      />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </Card>
   );
 }
