0f47a457eb3640dfcaa2248ac1a70ccd77249d46
YaChugunov <informed.looser@gmail.com>
2022-04-11 22:17:13 +0300

    1 file modified

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function CardImage(props) {
-  return (
-    <>
-      <img src={props.src} className="card-img-top" alt="" />
-    </>
-  );
-}
-
 function Card(props) {
+  console.log(props.children);
   return (
     <>
       <div class="card">
@@ -21,25 +14,6 @@ function Card(props) {
             {props.button}
           </a>
         </div>
-      </div>
-    </>
-  );
-}
-
-function CardB(props) {
-  const title = props.title;
-  const text = props.text;
-  return (
-    <>
-      <div class="card-body">
-        <h5 class="card-title">{title}</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
       </div>
     </>
   );
